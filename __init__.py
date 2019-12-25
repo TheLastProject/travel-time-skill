68a7832fedefbe5f42f2c010b99449cb7d3eb74d
Sylvia van Os <sylvia@hackerchick.me>
2019-12-25 20:29:43 +0100

    Also geocode from location

--- __init__.py
+++ __init__.py
@@ -10,6 +10,19 @@ class TravelTime(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    def _geocode(self, location):
+        # WazeRouteCalculator is not good at geocoding
+        # So we ask OpenStreetMap Nomatim
+        url = "https://nominatim.openstreetmap.org/search?{}".format(urlencode({'q': location, 'format': 'json', 'limit': 1}))
+        with urlopen(url) as data:
+            json_data = data.read().decode('utf-8')
+            parsed_data = json.loads(json_data)
+            try:
+                return "{},{}".format(parsed_data[0]["lat"], parsed_data[0]["lon"])
+            except (IndexError, KeyError):
+                return None
+
+
     @intent_file_handler('time.travel.intent')
     def handle_time_travel(self, message):
         destination_string = message.data.get('destination')
@@ -18,18 +31,18 @@ class TravelTime(MycroftSkill):
 
         # WazeRouteCalculator is not good at reverse geocoding
         # So we ask OpenStreetMap Nomatim first
-        url = "https://nominatim.openstreetmap.org/search?{}".format(urlencode({'q': destination_string, 'format': 'json', 'limit': 1}))
-        with urlopen(url) as data:
-            json_data = data.read().decode('utf-8')
-            parsed_data = json.loads(json_data)
-            try:
-                destination = "{},{}".format(parsed_data[0]["lat"], parsed_data[0]["lon"])
-            except (IndexError, KeyError):
-                self.speak_dialog('time.travel.failed_finding_location', {'location': destination_string})
-                return None
+        destination = self._geocode(destination_string)
+        if not destination:
+            self.speak_dialog('time.travel.failed_finding_location', {'location': destination_string})
+            return None
 
         from_string = message.data.get('from')
-        if not from_string:
+        if from_string:
+            from_ = self._geocode(from_string)
+            if not from_:
+                self.speak_dialog('time.travel.failed_finding_location', {'location': destination_string})
+                return None
+        else:
             from_string = self.location_pretty
             current_location = self.location["coordinate"]
             from_ = "{},{}".format(current_location["latitude"], current_location["longitude"])
